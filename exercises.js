0c575df0d6529d286c4ed7ff3bd40543fd622a72
lollollol1234 <51759218+lollollol1234@users.noreply.github.com>
2025-11-07 11:04:29 +0100

    Add initial exercises data structure

---
+++ exercises.js
@@ -0,0 +1,5 @@
+const EXERCISES = [
+  // --- Brust
+  { id: "pushup", name: "Liegestütz", primary: ["brust"], secondary: ["arme"], equipment: "Körpergewicht", difficulty: "Einsteiger", steps: ["Hände unter Schultern", "Körper gerade halten"], cues: ["Spannung halten"] },
+  // ... (alle anderen Übungen hier einfügen)
+];
